48b2bfcc04c8a4f3d18ba5c883a75a3fa571d4da
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-05-21 19:35:10 +0200

    -

---
+++ greet.js
@@ -0,0 +1,17 @@
+//reference the textarea
+const nameElem = document.querySelector(".textBox");
+//reference the button
+const greetBtn = document.querySelector(".button");
+// refrence the radio button
+const selectLanguage = document.querySelector(".languageRadio");
+
+
+
+
+    greetBtn.addEventListener("click", greet)
+
+
+   
+    
+
+
